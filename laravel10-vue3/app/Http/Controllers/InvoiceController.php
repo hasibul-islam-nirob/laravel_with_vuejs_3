85ded6be92f945c5eed2d3af866b2debe3451949
Hasibul Islam Nirob <hasibolislamnirob@gmail.com>
2023-10-24 23:43:29 +0600

    Display all Invoice

--- laravel10-vue3/app/Http/Controllers/InvoiceController.php
+++ laravel10-vue3/app/Http/Controllers/InvoiceController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
-    //
+    public function get_all_invoices(Request $request){
+
+        $invoice = Invoice::with('customers')->orderBy('id', 'DESC')->get();
+
+        return response()->json([
+            'invoice' => $invoice
+        ], 200);
+
+        $data = [
+            'invoice' => $invoice,
+            'status' => 200
+        ];
+        // return response()->json($data);
+
+    }
 }
--- laravel10-vue3/app/Models/Invoice.php
+++ laravel10-vue3/app/Models/Invoice.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Customers;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Invoice extends Model
 {
     use HasFactory;
+
+    public function customers(){
+        return $this->belongsTo(Customers::class);
+    }
+
 }
