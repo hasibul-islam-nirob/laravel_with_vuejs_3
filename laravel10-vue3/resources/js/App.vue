cf7fa6f66ab5a2caa306fb85cbdd7887326e178d
Hasibul Islam Nirob <hasibolislamnirob@gmail.com>
2023-10-24 12:34:31 +0600

    Create New Branch

--- laravel10-vue3/resources/js/App.vue
+++ laravel10-vue3/resources/js/App.vue
@@ -1,5 +1,5 @@
 <template>
     <h1>
-        Welcome VUE 3
+        Welcome VUE 3 ok
     </h1>
 </template>
--- laravel10-vue3/routes/web.php
+++ laravel10-vue3/routes/web.php
@@ -15,8 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('app');
-})
-->name('application');
+});
 
 // Route::get('/', function () {
 //     return view('welcome');
